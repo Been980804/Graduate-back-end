e7e811dab20d2e197a92dcd279cb615651d121d5
hblee <hyeonbeen980804@gmail.com>
2024-06-03 01:32:27 +0900

    schedule/compare get방식 -> post방식

--- src/main/java/graduate/cinemabackend/schedule/controller/ScheduleController.java
+++ src/main/java/graduate/cinemabackend/schedule/controller/ScheduleController.java
@@ -3,7 +3,7 @@ package graduate.cinemabackend.schedule.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +21,7 @@ public class ScheduleController {
     ScheduleService scheduleService;
     
     //영화 스케줄 비교
-    @GetMapping(value="/compare")
+    @PostMapping(value="/compare")
     public ResponseDTO compare(@RequestBody Map<String, Object> reqMap) {
         ResponseDTO res = scheduleService.compare(reqMap);
 
