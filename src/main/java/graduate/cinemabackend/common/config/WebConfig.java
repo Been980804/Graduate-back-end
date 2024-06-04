8eac9ea4076c9d9c7c33e886d10a65150652d0db
hblee <hyeonbeen980804@gmail.com>
2024-06-04 13:21:30 +0900

    WebConfig 추가

--- src/main/java/graduate/cinemabackend/common/config/WebConfig.java
+++ src/main/java/graduate/cinemabackend/common/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowCredentials(true)
-                .allowedOrigins("http://localhost:5173");
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
 
     }
 }
